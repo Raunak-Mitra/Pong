6b6efcd21721d40e85747b561f55c1eb4a1fedab
Raunak-Mitra <92012429+Raunak-Mitra@users.noreply.github.com>
2022-06-18 20:34:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,205 @@
+#from ast import Pass
+import pygame 
+from pygame.locals import *
+pygame.init()
+
+
+WIDTH,HEIGHT = 700,500
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Pong")
+
+paddle_width=20
+paddle_height=100
+ball_radius = 7
+
+FPS = 100
+
+white = (255,255,255)
+black = (0,0,0)
+
+SCORE_FONT = pygame.font.SysFont("arial",30)
+WINNING_SCORE = 5
+
+class Paddle:
+    COLOR = white #white defined as variable above 
+    VEL = 4
+    def __init__(self,x,y,width,height):
+        self.x =self.og_x= x
+        self.y = self.og_y =y
+        self.width = width
+        self.height = height 
+
+    def draw(self,win):
+        #pygame built in function to draw rectangle
+        pygame.draw.rect(win, self.COLOR,(self.x,self.y,self.width,self.height))
+
+    def move(self, up):
+        if up:
+            self.y-=self.VEL
+        else:
+            self.y+=self.VEL
+    
+    def reset(self):
+        self.x = self.og_x
+        self.y = self.og_y
+
+
+class Ball:
+    MAX_VEL = 4
+    COLOR = white 
+
+    def __init__(self,x,y,radius):
+        self.x= self.original_x = x
+        self.y= self.original_y = y
+        self.radius = radius 
+        self.x_vel= self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self,win):
+        pygame.draw.circle(win,self.COLOR,(self.x,self.y),self.radius)
+
+    def move(self):
+        self.x+=self.x_vel
+        self.y+=self.y_vel
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.x_vel *=-1
+        self.y_vel =0
+
+
+def handle_collision(ball,left_paddle,right_paddle):
+    if ball.y + ball.radius >=HEIGHT:
+        ball.y_vel*=-1
+    elif ball.y + ball.radius <= 0:
+        ball.y_vel*=-1
+
+    if ball.x_vel<0:
+        if ball.y>=left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x -ball.radius <=left_paddle.x +left_paddle.width:
+                ball.x_vel *=-1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                difference_in_y = middle_y - ball.y 
+                reduction_factor = (left_paddle.height / 2)/ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1* y_vel
+    else:
+        if ball.y>=right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x+ball.radius >= right_paddle.x:
+                ball.x_vel*=-1
+                middle_y = right_paddle.y + right_paddle.height / 2
+                difference_in_y = middle_y - ball.y 
+                reduction_factor = (right_paddle.height / 2)/ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel =-1* y_vel
+
+
+
+
+def handle_paddle_movement(keys,left_paddle,right_paddle):
+    if keys[pygame.K_w] and left_paddle.y-left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL+ left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y-right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL+ right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+
+
+def draw(win,paddles,ball,left_score,right_score):
+    win.fill(black)
+
+    left_score_text = SCORE_FONT.render("{}".format(left_score),True,white)
+    right_score_text = SCORE_FONT.render("{}".format(right_score),True,white)
+    win.blit(left_score_text,(WIDTH//4 - left_score_text.get_width()//2,20))
+    win.blit(right_score_text,(WIDTH*(3/4) - right_score_text.get_width()//2,20))
+
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+#lines in the middle 
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i%2 ==1:
+            continue
+        pygame.draw.rect(win,white,(WIDTH//2 -3, i , 6, HEIGHT//20))
+    
+    ball.draw(win)
+
+
+    pygame.display.update()
+
+
+def main():
+    running = True
+    clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10,HEIGHT//2 - paddle_height//2, paddle_width,paddle_height)
+    right_paddle = Paddle(WIDTH-10-paddle_width,HEIGHT//2-paddle_height//2, paddle_width,paddle_height)
+    ball = Ball(WIDTH//2,HEIGHT//2,ball_radius )
+
+    left_score =0
+    right_score =0
+
+    while running:
+        clock.tick(FPS)
+        draw(window,[left_paddle, right_paddle],ball,left_score,right_score)
+
+        for event in pygame.event.get():
+            """if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    ball.reset()
+                    left_paddle.reset()
+                    right_paddle.reset()
+                    left_score = 0 
+                    right_score = 0"""
+
+
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys,left_paddle,right_paddle)
+
+        ball.move()
+        handle_collision(ball,left_paddle,right_paddle)
+
+        if ball.x <0:
+            right_score+=1
+            ball.reset()
+        elif ball.x > WIDTH:
+            left_score+=1
+            ball.reset()
+
+        won = False 
+
+        if left_score >= WINNING_SCORE:
+            won = True
+            win_text = "Left Player Won!"
+        elif right_score >=WINNING_SCORE:
+            won = True
+            win_text = "Right Player Won!"
+
+        if won:
+            text = SCORE_FONT.render(win_text,True,(230,0,0))
+            window.blit(text,( WIDTH // 2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
+            pygame.display.update()
+            
+            pygame.time.delay(5000)
+            
+            ball.reset()
+            left_paddle.reset()
+            right_paddle.reset()
+            left_score = 0 
+            right_score = 0
+            
+    
+    pygame.quit()
+            
+if __name__ == '__main__':
+    main()
